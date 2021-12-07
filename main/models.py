99a3e9dc2d32904ae4a3bc241fc19113610b8319
stamixthereal <staskazanov@gmail.com>
2021-12-07 10:07:21 +0300

    delete one status of task from models

--- main/models.py
+++ main/models.py
@@ -6,19 +6,15 @@ from django.contrib.auth.models import User
 class Task(models.Model):
 
     STATUS_CHOICES = (
-                    ('Frozen', 'Frozen'),
                     ('Done', 'Done'),
                     ('Undone', 'Undone')
                     )
                     
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField('Name', max_length=255)
     task = models.TextField('Description')
     publish = models.DateTimeField(default=timezone.now)
-    status = models.CharField(
-                            max_length=10,
-                            choices=STATUS_CHOICES,
-                            default='Undone')
+    status = models.CharField(choices=STATUS_CHOICES, default='Undone')
 
     def __str__(self) -> str:
         return self.title
